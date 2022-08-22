0b60c6a5f618dbad74835fdc1c6a47f325c5543c
Michael Ogu <mickeybezalel@gmail.com>
2022-08-22 02:33:57 +0100

    chores: Project setup

---
+++ src/components/UWeatherWidget/UWeatherWidget.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UWeatherWidget = () => {
+  return <div>Unveel weather widget</div>;
+};
+
+export default UWeatherWidget;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as UWeatherWidget } from './UWeatherWidget';
